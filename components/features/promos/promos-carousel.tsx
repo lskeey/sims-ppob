be11bf2258e553189d41c680f49487066d25dbe4
lskeey <muhammd.alviansyah@gmail.com>
2025-09-08 11:13:27 +0700

    feat: add promos-carousel.tsx component

---
+++ components/features/promos/promos-carousel.tsx
@@ -0,0 +1,25 @@
+import { promotions } from "@/data/promotion";
+import Image from "next/image";
+
+export default function PromosCarousel() {
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="font-medium">Discover Exclusive Promos</h2>
+      </div>
+      <div className="flex gap-4 overflow-x-scroll">
+        {promotions.map((promotion, i) => (
+          <div key={i} className="w-56 flex-none">
+            <Image
+              src={"/images/promos/" + promotion.src}
+              alt={promotion.name}
+              width={500}
+              height={500}
+              priority
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
