3ad898df4feaf66657c7138b89dc7f93881291f9
lskeey <muhammd.alviansyah@gmail.com>
2025-09-09 07:43:57 +0700

    feat: enhance header functionality with additional code

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -9,17 +9,21 @@ import {
   SheetTrigger,
 } from "../ui/sheet";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="fixed flex items-center left-0 top-0 right-0 h-16 bg-background border-b z-50">
       <div className="flex items-center justify-between gap-2 container max-w-2xl lg:max-w-4xl mx-auto px-3">
-        <div className="w-auto flex-none flex items-center gap-1.5 select-none cursor-pointer">
+        <Link
+          href="/"
+          className="w-auto flex-none flex items-center gap-1.5 select-none cursor-pointer"
+        >
           <div className="w-auto p-1.5 bg-red-500 rounded-full">
             <Wallet size={16} color="white" />
           </div>
           <span className="shrink-0 font-medium">SIMS PPOB</span>
-        </div>
+        </Link>
         <div className="hidden lg:block">
           <NavMenu className="flex items-center" />
         </div>
--- components/nav-menu.tsx
+++ components/nav-menu.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface NavMenuProps {
   className?: string;
@@ -11,17 +14,23 @@ interface MenuItem {
 }
 
 const menuItems: MenuItem[] = [
-  { name: "Top Up", href: "#" },
-  { name: "Transactions", href: "#" },
-  { name: "Profile", href: "#" },
+  { name: "Top Up", href: "/topup" },
+  { name: "Transactions", href: "/transactions" },
+  { name: "Profile", href: "/profile" },
 ];
 
 export default function NavMenu({ className }: NavMenuProps) {
+  const pathname = usePathname();
   return (
     <nav>
       <ul className={cn("gap-6", className)}>
         {menuItems.map((item, index) => (
-          <li key={index} className="hover:text-red-600 cursor-pointer">
+          <li
+            key={index}
+            className={`hover:text-red-600 cursor-pointer ${
+              pathname === item.href ? "text-red-500" : ""
+            }`}
+          >
             <Link href={item.href}>{item.name}</Link>
           </li>
         ))}
