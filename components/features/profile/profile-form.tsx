f9591992a22f5da8dd0be3a61c589cf02d0716c3
lskeey <muhammd.alviansyah@gmail.com>
2025-09-10 14:07:34 +0700

    refactor: enhance profile form component with improved state handling

--- components/features/profile/profile-form.tsx
+++ components/features/profile/profile-form.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -9,21 +20,24 @@ import { useAuthStore } from "@/stores/authStore";
 import { useProfileStore } from "@/stores/profileStore";
 import { Label } from "@radix-ui/react-label";
 import { LogOut } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
-import { ClipLoader } from "react-spinners";
+import { IoIosWarning } from "react-icons/io";
+import { SyncLoader } from "react-spinners";
+import { toast } from "sonner";
 import z from "zod";
 
 export default function ProfileForm() {
   const router = useRouter();
+  const { profile, fetchProfile, updateProfile, loading, error } =
+    useProfileStore();
   const [isEditing, setIsEditing] = useState(false);
-  const [errors, setErrors] = useState<Partial<UpdateProfileRequest>>({});
   const [formValues, setFormValues] = useState<UpdateProfileRequest>({
     first_name: "",
     last_name: "",
   });
-  const { profile, fetchProfile, updateProfile, loading, error } =
-    useProfileStore();
+  const [errors, setErrors] = useState<Partial<UpdateProfileRequest>>({});
   const { logout } = useAuthStore();
 
   const handleEditToggle = useCallback(
@@ -58,48 +72,40 @@ export default function ProfileForm() {
     );
   }, [formValues, profile]);
 
-  const handleSubmit = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      setErrors({});
+  const handleSubmit = useCallback(async () => {
+    setErrors({});
 
-      if (!hasChanges()) {
-        setIsEditing(false);
-        return;
+    try {
+      await updateProfile(formValues);
+      toast("Profile has been updated.", {
+        style: {
+          backgroundColor: "#00bc7d",
+          color: "#fff",
+        },
+      });
+      setIsEditing(false);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErrors: { first_name?: string; last_name?: string } = {};
+        error.issues.forEach((err) => {
+          if (err.path[0] === "first_name") {
+            fieldErrors.first_name = err.message;
+          } else if (err.path[0] === "last_name") {
+            fieldErrors.last_name = err.message;
+          }
+        });
+        setErrors(fieldErrors);
+      } else {
+        const errorMessage =
+          error instanceof Error
+            ? error.message.includes("401")
+              ? "Unauthorized access. Please log in again."
+              : error.message
+            : "An unexpected error occurred. Please try again.";
+        console.log(errorMessage);
       }
-
-      try {
-        await updateProfile(formValues);
-        setIsEditing(false);
-      } catch (error) {
-        if (error instanceof z.ZodError) {
-          const fieldErrors: { first_name?: string; last_name?: string } = {};
-          error.issues.forEach((err) => {
-            if (err.path[0] === "first_name") {
-              fieldErrors.first_name = err.message;
-            } else if (err.path[0] === "last_name") {
-              fieldErrors.last_name = err.message;
-            }
-          });
-          setErrors(fieldErrors);
-        } else {
-          const errorMessage =
-            error instanceof Error
-              ? error.message.includes("401")
-                ? "Unauthorized access. Please log in again."
-                : error.message
-              : "An unexpected error occurred. Please try again.";
-          console.log(errorMessage);
-        }
-      }
-    },
-    [formValues, hasChanges, updateProfile, logout, router]
-  );
-
-  const handleLogout = useCallback(() => {
-    logout();
-    router.push("/auth/login");
-  }, [logout, router]);
+    }
+  }, [formValues, hasChanges, updateProfile, logout, router]);
 
   useEffect(() => {
     fetchProfile();
@@ -114,24 +120,45 @@ export default function ProfileForm() {
     }
   }, [profile]);
 
-  if (!profile && loading) {
+  const handleLogout = useCallback(() => {
+    logout();
+    router.push("/auth/login");
+  }, [logout, router]);
+
+  if (loading || !profile) {
     return (
-      <div className="flex justify-center items-center">
-        <ClipLoader size={32} color="#ef4444" />
+      <div
+        className="flex justify-center items-center"
+        style={{ height: "calc(100vh - 12rem)" }}
+      >
+        <SyncLoader size={16} color="#fb2c36" />
       </div>
     );
   }
 
   if (!profile) {
     return (
-      <div className="text-center p-6">
-        <p className="text-red-500">Failed to load profile. {error}</p>
+      <div className="flex flex-col items-center justify-center px-8 py-6 text-center space-y-6">
+        <div className="w-64">
+          <Image
+            src="/images/warning-pana.svg"
+            alt="Warning Illustration"
+            width={500}
+            height={500}
+          />
+        </div>
+        <p className="text-md">
+          Something went wrong. Couldn't load the profile.
+        </p>
+        <button className="flex items-center gap-2 bg-red-500 text-background px-2 py-1 rounded-sm cursor-pointer hover:bg-red-600">
+          Retry
+        </button>
       </div>
     );
   }
 
   return (
-    <form className="space-y-12" onSubmit={handleSubmit}>
+    <form className="space-y-12">
       <div className="flex flex-col items-center gap-3">
         <Avatar className="size-24">
           <AvatarFallback className="text-4xl text-background bg-red-500">
@@ -196,17 +223,62 @@ export default function ProfileForm() {
             <p className="text-red-500 text-xs">{errors.last_name}</p>
           )}
         </div>
-        <Button
-          type={isEditing ? "submit" : "button"}
-          onClick={isEditing ? undefined : handleEditToggle}
-          className="w-full rounded-sm bg-red-500 hover:bg-red-600 cursor-pointer"
-        >
-          {isEditing ? (hasChanges() ? "Save" : "Cancel") : "Edit"}
-        </Button>
+        {isEditing ? (
+          <>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  className="text-background bg-red-500 hover:bg-red-600"
+                  disabled={!hasChanges()}
+                >
+                  Save
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent className="w-sm space-y-6">
+                <AlertDialogHeader>
+                  <IoIosWarning className="self-center size-24 text-yellow-500" />
+                  <AlertDialogTitle>Are You Sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action will apply permanent changes that can't be
+                    undone.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <div className="flex gap-6">
+                    <AlertDialogCancel className="flex-1 cursor-pointer">
+                      Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={handleSubmit}
+                      className="flex-1 bg-red-500 hover:bg-red-600 cursor-pointer"
+                    >
+                      Confirm
+                    </AlertDialogAction>
+                  </div>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+            <Button
+              type="button"
+              className="bg-background border border-red-500 text-red-500 hover:bg-red-50"
+              onClick={handleEditToggle}
+            >
+              Cancel
+            </Button>
+          </>
+        ) : (
+          <Button
+            type="button"
+            className="bg-red-500 hover:bg-red-600 text-background"
+            onClick={handleEditToggle}
+          >
+            Edit
+          </Button>
+        )}
         {!isEditing && (
           <Button
             type="button"
-            className="w-full rounded-sm text-red-500 hover:text-background border border-red-500 bg-background hover:bg-red-600 cursor-pointer"
+            className="w-full rounded-sm text-red-500 border border-red-500 bg-background hover:bg-red-50 "
             onClick={handleLogout}
           >
             <span>
