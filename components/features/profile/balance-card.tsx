e36c4a0f1fea51b3ac6d313115486a5c01c471f6
lskeey <muhammd.alviansyah@gmail.com>
2025-09-10 09:14:27 +0700

    refactor: enhance profile and balance card component with improved state handling

--- components/features/profile/balance-card.tsx
+++ components/features/profile/balance-card.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { formatNumber } from "@/lib/utils";
 import { useBalanceStore } from "@/stores/balanceStore";
 import { Eye, EyeOff } from "lucide-react";
@@ -8,12 +9,17 @@ import { useEffect, useState } from "react";
 export default function BalanceCard() {
   const { balance, fetchBalance, loading, error } = useBalanceStore();
   const [showBalance, setShowBalance] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (balance === null && !loading && !error) {
+      fetchBalance();
+    }
+  }, [balance, loading, error, fetchBalance]);
+
   const toggleBalance = (): void => {
     setShowBalance(!showBalance);
   };
-  const balanceDiv = (
-    <span className="text-3xl">{formatNumber(balance || 0)}</span>
-  );
+
   const hiddenBalance = (
     <span className="flex items-center gap-1.5 ml-2">
       {Array.from({ length: 6 }).map((_, index) => (
@@ -22,36 +28,61 @@ export default function BalanceCard() {
     </span>
   );
 
-  useEffect(() => {
-    fetchBalance();
-  }, [fetchBalance]);
+  const renderBalance = () => {
+    if (loading) {
+      return <Skeleton className="w-54 h-6 bg-black/20" />;
+    }
+
+    if (error) {
+      return;
+    }
+
+    if (balance) {
+      return (
+        <>
+          <span className="text-lg">Rp</span>
+          <span className="text-3xl">
+            {showBalance ? formatNumber(balance) : hiddenBalance}
+          </span>
+        </>
+      );
+    }
+  };
+
   return (
     <div className="flex flex-col justify-between h-32 text-background bg-red-500 p-3 lg:p-4 rounded-xl min-w-max">
-      <div className="">
-        <h2 className="text-sm">Balance</h2>
+      <div>
+        {loading ? (
+          <Skeleton className="w-20 h-4 bg-black/20 rounded-sm" />
+        ) : (
+          <h2 className="text-sm">Balance</h2>
+        )}
       </div>
-      <div className="">
-        <p className="flex items-center font-semibold">
-          <span className="text-lg">Rp</span>
-          {showBalance ? balanceDiv : hiddenBalance}
-        </p>
+      <div>
+        <span className="flex items-center font-semibold">
+          {renderBalance()}
+        </span>
       </div>
-      <div className="">
-        <button
-          onClick={toggleBalance}
-          className="flex items-center gap-1 text-xs cursor-pointer"
-          aria-expanded={showBalance ? "true" : "false"}
-          aria-label="Toggle balance visibility"
-        >
-          <span>{showBalance ? "Hide Balance" : "Show Balance"}</span>
-          <span>
-            {showBalance ? (
-              <EyeOff strokeWidth={1.5} size={16} />
-            ) : (
-              <Eye strokeWidth={1.5} size={16} />
-            )}
-          </span>
-        </button>
+      <div>
+        {loading ? (
+          <Skeleton className="w-24 h-4 bg-black/20 rounded-sm" />
+        ) : (
+          <button
+            onClick={toggleBalance}
+            className="flex items-center gap-1 text-xs cursor-pointer"
+            aria-expanded={showBalance ? "true" : "false"}
+            aria-label="Toggle balance visibility"
+          >
+            <span>{showBalance ? "Hide Balance" : "Show Balance"}</span>
+            <span>
+              {showBalance ? (
+                <EyeOff strokeWidth={1.5} size={16} />
+              ) : (
+                <Eye strokeWidth={1.5} size={16} />
+              )}
+            </span>
+          </button>
+        )}
       </div>
     </div>
   );
--- components/features/profile/profile-card.tsx
+++ components/features/profile/profile-card.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { getInitials } from "@/lib/utils";
 import { useProfileStore } from "@/stores/profileStore";
 import { useEffect } from "react";
@@ -11,24 +12,33 @@ export default function ProfileCard() {
     fetchProfile();
   }, [fetchProfile]);
 
-  if (!profile) {
-    return null;
-  }
-
   return (
-    <div className="flex gap-2 lg:flex-col lg:gap-4">
+    <div className="flex gap-2 lg:flex-col lg:gap-4 h-full lg:justify-between">
       <div className="flex-none">
-        <Avatar className="size-12">
-          <AvatarFallback className="bg-red-500 text-background">
-            {getInitials(profile.first_name, profile.last_name)}
-          </AvatarFallback>
-        </Avatar>
+        {!profile ? (
+          <Skeleton className="size-12 bg-black/10 rounded-full" />
+        ) : (
+          <Avatar className="size-12">
+            <AvatarFallback className="bg-red-500 text-background">
+              {getInitials(profile.first_name, profile.last_name)}
+            </AvatarFallback>
+          </Avatar>
+        )}
       </div>
       <div className="flex flex-1 items-center lg:block">
-        <span className="hidden lg:inline">Welcome,</span>
-        <h2 className="text-lg font-medium text-nowrap lg:text-2xl">
-          {profile.first_name + " " + profile.last_name}
-        </h2>
+        {!profile ? (
+          <div className="lg:space-y-2">
+            <Skeleton className="hidden lg:block w-20 h-4 bg-black/10 rounded-full" />
+            <Skeleton className="w-56 h-6 bg-black/10 rounded-full" />
+          </div>
+        ) : (
+          <>
+            <span className="hidden lg:inline">Welcome,</span>
+            <h2 className="text-lg font-medium text-nowrap lg:text-2xl">
+              {profile.first_name + " " + profile.last_name}
+            </h2>
+          </>
+        )}
       </div>
     </div>
   );
